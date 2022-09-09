1f20dcc7ca963529558f46a149a937b75b1069dc
Edwin Lutaaya <gkedwin1@gmail.com>
2022-09-09 03:03:54 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,16 @@
-#iclude<studio.h>
+
+#include <stdio.h>
 
 /**
- * main - printing 
- *
- * Return: Always 0 (Successful)
- * /
- int main(void)
- {
-
-        puts("\"Programming is like building a multiligual puzzle");
+* main - print the string in the put function
+*
+* Description: using the main function
+* this program prints "Programming is like building a multilingual puzzle
+* Return: 0
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
+}
 
-	}
