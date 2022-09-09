9dac7ef513e9effed21bc606456bc3982713b14e
Edwin Lutaaya <gkedwin1@gmail.com>
2022-09-09 06:17:45 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,40 +1,9 @@
-root@463b0ef79ff1:~/alx-low_level_programming/0x00-hello_world# ls
-0-preprocessor  2-assembler  4-puts.c    README.md
-1-compiler      3-name       5-printf.c
-root@463b0ef79ff1:~/alx-low_level_programming/0x00-hello_world# betty 5-printf.c
- 
-========== 5-printf.c ==========
-5-printf.c:3: WARNING: space prohibited between function name and open parenthesis '('
-5-printf.c:4: ERROR: trailing whitespace
-5-printf.c:4: WARNING: please, no spaces at the start of a line
-5-printf.c:5: WARNING: please, no spaces at the start of a line
-5-printf.c:6: ERROR: trailing whitespace
-5-printf.c:6: WARNING: please, no spaces at the start of a line
-5-printf.c:7: WARNING: please, no spaces at the start of a line
-5-printf.c:7: ERROR: parentheses are required on a return statement
-total: 3 errors, 5 warnings, 8 lines checked
-5-printf.c:3: warning: no description found for function main
-root@463b0ef79ff1:~/alx-low_level_programming/0x00-hello_world# vi 4-puts.c
-
-
-
-
-
-
-#include <studio.h>
-
-/**
-*main - execution begins here
-*
-*description: "Programming is like building a multilingual puzzle
-*
-*Return: Return 0 for success
-*/
-
-int main(void)
-{     
-       puts("\"with proper grammar, but the outcome is a piece of art,")
-return (0); 
+#include <stdio.h>
+int main (){
+    
+    printf("with proper grammar, but the outcome is a piece of art,");
+    
+    return 0;
 }   
 ~                                                                               
 ~                                                                               
