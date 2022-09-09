ef7dd9c813bd023a28583e47c472d5559b29421d
Edwin Lutaaya <gkedwin1@gmail.com>
2022-09-09 04:53:37 -0700

    the prompts from online

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,14 +1,7 @@
-#include <stdio>
-
-/**
-* main - print the string in the put function
-*
-* Description: using the main function 
-* this program prints "with proper grammar, but the outcome is a piece of art
-* Return: 0
-*/
-int main(void)
-{
-       printf("with proper grammar, but the outcome is a piece of art,\n");
-return (0);
+#include <stdio.h>
+int main (){
+    
+    printf("with proper grammar, but the outcome is a piece of art,");
+    
+    return 0;
 }
