4d98118581cdca1d09acc12875322c7fdb886284
Edwin Lutaaya <gkedwin1@gmail.com>
2022-09-10 01:10:59 -0700

    5-Printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,15 @@
-#include <stdio.h>
-int main (){
-    
-    printf("with proper grammar, but the outcome is a piece of art,");
-    
-    return 0;
-}
+#include <studio.h>
+
+/**
+*main - execution begins here
+*
+*description: "Programming is like building a multilingual puzzle
+*
+*Return: Return 0 for success
+*/      
+
+int main(void)
+{     
+       puts("\"with proper grammar, but the outcome is a piece of art,")
+return (0); 
+}  
