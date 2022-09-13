ecbcd7a77d74b7965904d10f0c94d4b8068eb7a2
Edwin Lutaaya <gkedwin1@gmail.com>
2022-09-13 11:09:30 -0700

    0-putchar.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -8,8 +8,9 @@
 *Return: Return 0 for success
 */      
 
-int main(void)
-{     
-       puts("\"with proper grammar, but the outcome is a piece of art,")
+int main(void) {     
+       
+	puts("\n"with proper grammar, but the outcome is a piece of art");
+		  
 return (0); 
 }  
