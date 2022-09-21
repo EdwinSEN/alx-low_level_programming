89fa17523f3710666f20a924c6dbe558e75d4647
Edwin Lutaaya <gkedwin1@gmail.com>
2022-09-21 14:23:55 -0700

    new fix2

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -6,15 +6,12 @@
  * @src: param pointer to a char
  * Return: return value of dest
  */
-
 char *_strcat(char *dest, char *src)
 {
 	int i;
 	int j;
 
-	i = 0;
-	j = 0;
-
+	j = 0;	
 	while (dest[i] != '\0')
 	{
 		i++;
@@ -25,8 +22,6 @@ char *_strcat(char *dest, char *src)
 		j++;
 		i++;
 	}
-
 	dest[i] = '\0';
 	return (dest);
 }
-	
