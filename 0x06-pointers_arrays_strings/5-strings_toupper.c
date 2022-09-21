ca806935b835dfcf7e9dc999d988524d607b7b21
Edwin Lutaaya <gkedwin1@gmail.com>
2022-09-21 15:38:40 -0700

    5-string_toupper.c hot fix

---
+++ 0x06-pointers_arrays_strings/5-strings_toupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ *  string_toupper -> converting any string to upper case
+ *  @x: string param
+ *  Return: string 
+ *  /
+char *string_toupper(char *X)
+{
+        int i = 0;
+
+        while (x[i])
+
+             if (X[i] >=97 && x[i] <= 122)
+                 X[i] = x[i] - 32;
+               i++;
+        return (s);
+}
