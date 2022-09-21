fbc9e29fe8b2010b602da6c9fc9cdfcff9d05135
Edwin Lutaaya <gkedwin1@gmail.com>
2022-09-21 13:17:02 -0700

    TASK 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,25 +1,25 @@
 #include "main.h"
-#include <string.h> 
+#include <string.h>
 
 /**
-*_strcat -> this is a function strcat
+*_strcat -> this is a function strcat 
 * @dest: first param
 * @sre: second param 
 * Return: a string
-*/
+*/       
 char *_streat(char *dest, char *src)
-{
-
+{             
      int len = 0, i;
-      
-     while (dest[len])
-        len++;
 
+     while (dest[len])
+        len++; 
+    
      for (i = 0; src[i] != 0; i++)
-     {
-	  dest[len] = src[i];
-          len += 1;
-     }
+     { 
+          dest[len] = src[i];
+          len += 1; 
+     } 
      dest[len] = '\0';
-    return (dest);
-}    
+ return (dest) 
+}	 1,6           Top
+
