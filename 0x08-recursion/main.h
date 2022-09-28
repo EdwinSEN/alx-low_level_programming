640a79231d013df9a5c3fa4b72024020ad900f59
EdwiNSEN <gkedwin1@gmail.com>
2022-09-28 02:11:52 -0700

    0-puts_recursion.c hot fix

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,10 +1,18 @@
-0. She locked away a secret, deep inside herself, something she once knew to be true... but chose to forget
-1. Why is it so important to dream? Because, in my dreams we are together
-2. Dreams feel real while we're in them. It's only when we wake up that we realize something was actually strange
-3. You mustn't be afraid to dream a little bigger, darling
-4. Once an idea has taken hold of the brain it's almost impossible to eradicate
-5. Your subconscious is looking for the dreamer
-6. Inception. Is it possible?
-7. They say we only use a fraction of our brain's true potential. Now that's when we're awake. When we're asleep, we can do almost anything
-8. Inception. Now, before you bother telling me it's impossible...
+#ifndef MAIN_H
+#define MAIN_H
 
+#include <stdio.h>
+
+int _putchar(char c);
+void _puts_recursion(char *s);
+void _print_rev_recursion(chr *s);
+int _strlen_recursion(char *s);
+int factorial(int n);
+int _pow_recursion(int x, int y);
+int _sqrt_recursion(int n);
+int is_prime_number(int n);
+int _sqrt(int n, int start);
+void check(char *s, int  start, int end, int *flag);
+int wildcmp(char *s1, char *s2);
+
+#endif /MAIN_H*/
