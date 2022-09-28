9349bb2a0b96fe133780501b19f4cb7ddfa7be0e
EdwiNSEN <gkedwin1@gmail.com>
2022-09-28 02:36:37 -0700

    1-print_rev_recursion.c hot fix

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,16 +1,15 @@
-include "main.h"
+#include "main.h"
 
 /**
-* _print_rev_recursion - print a string in reverse
-* @s: string to be printed
-* Return: void
-*/
-void _print-rev_recursion(char *s)
-{
-        if (*s != '\0')
-        {
-                    _print_rev_recursion(s + 1);;
-                    _putchar(*s);
-        }
-}
+ * _print_rev_recursion - print string in reverse
+ * @s: string to print
+ */
 
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+		return;
+
+	_print_rev_recursion(s + 1);
+	_putchar(*s);
+}
