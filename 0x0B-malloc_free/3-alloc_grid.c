deb2f8328f476dc1d294200c0c4bf7ec16735454
EdwiNSEN <gkedwin1@gmail.com>
2022-10-04 05:09:37 -0700

    task 3 hot fix

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,40 +1,52 @@
-#include "main.h"
-#include <stdio.h>
+/*
+ * File: 3-alloc_grid.c
+ * Auth: Brennan D Baraban
+ */
+
+#include "holberton.h"
 #include <stdlib.h>
+
 /**
- * alloc_grid - creates matrix
- * @width: width of array
- * @height: height of array
- * Return: pointer of new grid
+ * alloc_grid - Returns a pointer to a 2-dimensional array of
+ *               integers with each element initalized to 0.
+ * @width: The width of the 2-dimensional array.
+ * @height: The height of the 2-dimensional array.
+ *
+ * Return: If width <= 0, height <= 0, or the function fails - NULL.
+ *         Otherwise - a pointer to the 2-dimensional array of integers.
  */
 int **alloc_grid(int width, int height)
 {
-	int **t;
-	int i;
-	int j;
+	int **twoD;
+	int hgt_index, wid_index;
 
 	if (width <= 0 || height <= 0)
 		return (NULL);
 
-	t = (int **)malloc(sizeof(int *) * height);
-	if (t == NULL)
+	twoD = malloc(sizeof(int *) * height);
+
+	if (twoD == NULL)
 		return (NULL);
 
-	for (i = 0; i < height; i++)
+	for (hgt_index = 0; hgt_index < height; hgt_index++)
 	{
-		t[i] = (int *)malloc(sizeof(int) * width);
-		if (t[i] == NULL)
+		twoD[hgt_index] = malloc(sizeof(int) * width);
+
+		if (twoD[hgt_index] == NULL)
 		{
-			for (j = 0; j < i; j++)
-			{
-				free(t[j]);
-			}
-			free(t);
+			for (; hgt_index >= 0; hgt_index--)
+				free(twoD[hgt_index]);
+
+			free(twoD);
+			return (NULL);
 		}
 	}
 
-	for (i = 0; i < height; i++)
-		for (j = 0; j < width; j++)
-			t[i][j] = 0;
+	for (hgt_index = 0; hgt_index < height; hgt_index++)
+	{
+		for (wid_index = 0; wid_index < width; wid_index++)
+			twoD[hgt_index][wid_index] = 0;
+	}
 
-	return (t);
+	return (twoD);
+}
