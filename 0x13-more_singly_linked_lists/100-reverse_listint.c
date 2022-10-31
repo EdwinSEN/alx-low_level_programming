0739d683a0c066e250285aa19db10c8635bce80e
EdwiNSEN <gkedwin1@gmail.com>
2022-10-31 14:53:09 -0700

    0x13. C - More singly linked lists

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -1,37 +1,27 @@
 #include "lists.h"
-#include <stdlib.h>
 
 /**
- * delete_nodeint_at_index - deletes the node at index index
- * of a listint_t linked list.
- * @head: double pointer to the first node in the list
- * @index: index of the node to delete
+ * reverse_listint - reverses a listint_t linked list.
+ * @head: double pointer to the start of the list
  *
- * Return: pointer to the indexed node
+ * Return: a pointer to the first node of the reversed list
  */
-int delete_nodeint_at_index(listint_t **head, unsigned int index)
+listint_t *reverse_listint(listint_t **head)
 {
-	unsigned int i;
-	listint_t *current, *next;
+	listint_t *previous, *next;
 
 	if (head == NULL || *head == NULL)
-		return (-1);
-	if (index == 0)
+		return (NULL);
+	if ((*head)->next == NULL)
+		return (*head);
+	previous = NULL;
+	while (*head != NULL)
 	{
 		next = (*head)->next;
-		free(*head);
+		(*head)->next = previous;
+		previous = *head;
 		*head = next;
-		return (1);
 	}
-	current = *head;
-	for (i = 0; i < index - 1; i++)
-	{
-		if (current->next == NULL)
-			return (-1);
-		current = current->next;
-	}
-	next = current->next;
-	current->next = next->next;
-	free(next);
-	return (1);
+	*head = previous;
+	return (*head);
 }
