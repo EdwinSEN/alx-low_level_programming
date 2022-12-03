92bd887ea8a11e97e3b32f3af24f97657281b431
EdwiNSEN <gkedwin1@gmail.com>
2022-12-03 05:45:56 -0800

    0x08-recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,18 +1,14 @@
-#include "main.h"
-
+#include "holberton.h"
 /**
- * _puts_recursion - print string with new line
- * @s: string to print
- */
-
-void _puts_recursion(char *s)
+ * _print_rev_recursion -  prints a string, in reverse.
+ * @s: pointer to string
+ * Return: nothing
+ **/
+void _print_rev_recursion(char *s)
 {
-	if (*s == '\0')
+	if (*s != '\0')
 	{
-		_putchar('\n');
-		return;
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
 	}
-	_putchar(*s);
-	s++;
-	_puts_recursion(s);
 }
--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,15 +1,14 @@
-#include "main.h"
-
+#include "holberton.h"
 /**
- * _print_rev_recursion - print string in reverse
- * @s: string to print
- */
-
+ * _print_rev_recursion -  prints a string, in reverse.
+ * @s: pointer to string
+ * Return: nothing
+ **/
 void _print_rev_recursion(char *s)
 {
-	if (*s == '\0')
-		return;
-
-	_print_rev_recursion(s + 1);
-	_putchar(*s);
+	if (*s != '\0')
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
 }
